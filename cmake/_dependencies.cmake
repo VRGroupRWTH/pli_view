e2a6581e2a05e7bbbb22eb0e644f27ae6720afba
Demiralp <demiralpali@gmail.com>
2017-01-15 21:58:16 +0100

    Added VTK dependency.

--- cmake/_dependencies.cmake
+++ cmake/_dependencies.cmake
@@ -12,6 +12,11 @@ set(CMAKE_INCLUDE_CURRENT_DIR ON)
 find_package(Qt5Widgets REQUIRED)
 set(ProjectLibraries ${ProjectLibraries} Qt5::Widgets)
 
+# Include VTK.
+find_package       (VTK REQUIRED)
+include_directories(${VTK_INCLUDE_DIRS})
+set(ProjectLibraries ${ProjectLibraries} ${VTK_LIBRARIES})
+
 # Include pli_io.
 include_directories(${pli_io_INCLUDE_DIR})
 set(ProjectLibraries ${ProjectLibraries} ${pli_io_LIBRARIES})
