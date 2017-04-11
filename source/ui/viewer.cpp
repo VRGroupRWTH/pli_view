d6238535d3cffbeae8a51be4ec2622d06d83c40e
demiralp <demiralp@vr.rwth-aachen.de>
2017-04-11 16:21:22 +0200

    Spinner no longer blocks the viewer during load.

--- source/ui/viewer.cpp
+++ source/ui/viewer.cpp
@@ -7,7 +7,7 @@
 
 namespace pli
 {
-viewer::viewer(QWidget* parent) : QOpenGLWidget(parent), interactor_(&camera_), wait_spinner_(new wait_spinner(this))
+viewer::viewer(QWidget* parent) : QOpenGLWidget(parent), interactor_(&camera_), wait_spinner_(new wait_spinner(this, true, false))
 {
   // Make adjustable.
   interactor_.set_move_speed(0.001);
