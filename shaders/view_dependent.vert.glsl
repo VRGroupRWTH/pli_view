3d2f83a2ae5cf10d1e207992080d4d4dc7bb0dd3
Demiralp <demiralpali@gmail.com>
2017-07-27 09:58:30 +0200

    Alpha depends on inverse of view matrix instead of view projection.

--- shaders/view_dependent.vert.glsl
+++ shaders/view_dependent.vert.glsl
@@ -21,7 +21,7 @@ void main()
 
   if(view_dependent)
   {
-    float alpha = 1.0 - abs(dot(normalize(inverse(projection * view)[2].xyz), normalize(color.xzy)));
+    float alpha = 1.0 - abs(dot(normalize(inverse(view)[2].xyz), normalize(color.xzy)));
     vert_color = vec4(color.xyz, alpha);
   }
   else
