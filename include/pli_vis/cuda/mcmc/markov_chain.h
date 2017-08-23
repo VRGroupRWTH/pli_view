2a0641bad5656cee1abf789c8a8e0434014057ad
demiralp <demiralp@vr.rwth-aachen.de>
2017-08-23 18:15:50 +0200

    Added markov chain stubs.

---
+++ include/pli_vis/cuda/mcmc/markov_chain.h
@@ -0,0 +1,24 @@
+#ifndef PLI_VIS_MARKOV_CHAIN_H_
+#define PLI_VIS_MARKOV_CHAIN_H_
+
+namespace pli
+{
+template<typename data_type>
+void setup_model(
+  unsigned   matrix_dimensions,
+  data_type* transition_matrix, // NxN
+  data_type* initial_matrix   ) // 1xN
+{
+  
+}
+
+template<typename data_type>
+void generate_sequence(
+  unsigned   size    ,
+  data_type* sequence)
+{
+  
+}
+}
+
+#endif
