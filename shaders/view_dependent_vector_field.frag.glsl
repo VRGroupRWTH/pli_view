e76d3f1e55e7c7a8ecc39a9dfad9b368c5eaed9f
Demiralp <demiralpali@gmail.com>
2017-09-17 19:00:21 +0200

    Added HSL HSV converters to view dependent shader.

--- shaders/view_dependent_vector_field.frag.glsl
+++ shaders/view_dependent_vector_field.frag.glsl
@@ -8,32 +8,35 @@ namespace shaders
 static std::string view_dependent_vector_field_frag = R"(\
 #version 450
 
-uniform bool  hsv            = true;
-uniform bool  view_dependent = true;
-uniform bool  invert         = true;
-uniform float rate_of_decay  = 1.0 ;
-uniform float cutoff         = 0.25;
+uniform bool  hsv            = true ;
+uniform bool  view_dependent = false;
+uniform bool  invert         = true ;
+uniform float rate_of_decay  = 1.0  ;
+uniform float cutoff         = 0.25 ;
 uniform mat4  view           ;
 out     vec4  frag_color     ;
 
 in vertex_data {
   flat vec3 direction;
 } fs_in;
- 
-vec3 rgb_to_hsv(vec3 color)
+
+vec3 hue_to_rgb(float hue)
 {
-  vec4  k = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
-  vec4  p = mix(vec4(color.bg, k.wz), vec4(color.gb, k.xy), step(color.b, color.g));
-  vec4  q = mix(vec4(p.xyw, color.r), vec4(color.r, p.yzx), step(p.x, color.r));
-  float d = q.x - min(q.w, q.y);
-  float e = 1.0e-10;
-  return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+  float R = abs(hue * 6 - 3) - 1;
+  float G = 2 - abs(hue * 6 - 2);
+  float B = 2 - abs(hue * 6 - 4);
+  return clamp(vec3(R,G,B), 0, 1);
 }
-vec3 hsv_to_rgb(vec3 color)
+vec3 hsv_to_rgb(vec3 hsv)
 {
-  vec4 k = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
-  vec3 p = abs(fract(color.xxx + k.xyz) * 6.0 - k.www);
-  return color.z * mix(k.xxx, clamp(p - k.xxx, 0.0, 1.0), color.y);
+  vec3 rgb = hue_to_rgb(hsv.x);
+  return ((rgb - 1.0) * hsv.y + 1.0) * hsv.z;
+}
+vec3 hsl_to_rgb(vec3 hsl)
+{
+  vec3 rgb = hue_to_rgb(hsl.x);
+  float C = (1 - abs(2 * hsl.z - 1)) * hsl.y;
+  return (rgb - 0.5) * C + hsl.z;
 }
 
 vec3 to_spherical(vec3 cartesian)
@@ -53,23 +56,20 @@ vec3 to_cartesian(vec3 spherical)
 
 void main()
 {
+  // RGB, HSv, HsV, HSl, HsL + all inverted.
   vec3 spherical = to_spherical(fs_in.direction);
-  if(spherical.y < 0.0)
-     spherical.y += radians(180.0);
-  if(spherical.y >= radians(180.0))
-     spherical.y -= radians(180.0);
+  if(spherical.y <  0.0)            spherical.y += radians(180.0);
+  if(spherical.y >= radians(180.0)) spherical.y -= radians(180.0);
   spherical.y = radians(180.0) - spherical.y;
 
-  if(spherical.z < 0.0)
-     spherical.z = abs(spherical.z);
-  if(spherical.z >= radians( 90.0))
-     spherical.z = radians(180.0) - spherical.z;
+  if(spherical.z < 0.0)             spherical.z = abs(spherical.z);
+  if(spherical.z >= radians( 90.0)) spherical.z = radians(180.0) - spherical.z;
 
   float hue        = (spherical.y / radians(180.0));
   float saturation = (spherical.z / radians( 90.0));
-  float value      = 1.0;
+  float value      = 0.5;
   
-  if(hsv) frag_color = vec4(hsv_to_rgb(vec3(hue, value, saturation)), 1.0);
+  if(hsv) frag_color = vec4(hsl_to_rgb(vec3(hue, value, saturation)), 1.0);
   else    frag_color = vec4(abs(fs_in.direction.x), abs(fs_in.direction.z), abs(fs_in.direction.y), 1.0);
 
   if(view_dependent)
