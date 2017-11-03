d7f7dea34a1200d7a03058b7bcffeaacb186a663
Demiralp <demiralpali@gmail.com>
2017-11-03 21:10:53 +0100

    Fix indexing.

--- include/pli_vis/cuda/zernike/disk.h
+++ include/pli_vis/cuda/zernike/disk.h
@@ -19,13 +19,13 @@ __global__ void sample_disk(const uint2& dimensions, precision* samples)
 {
   const auto x = blockIdx.x * blockDim.x + threadIdx.x;
   const auto y = blockIdx.y * blockDim.y + threadIdx.y;
-  if(x > dimensions.x || y >= dimensions.y)
+  if(x >= dimensions.x || y >= dimensions.y)
     return;
 
   const auto rho   =      sqrtf(x / dimensions.x);
   const auto theta = 2 * M_PI * y / dimensions.y; 
 
-  const auto sample_index = y * dimensions.x + x;
+  const auto sample_index = y + dimensions.y * x;
   samples[sample_index].x = rho * cos(theta);
   samples[sample_index].y = rho * sin(theta);
 }
