6a22a0cfa1048d10fcf3677aa3bf666d16719592
demiralp <demiralp@vr.rwth-aachen.de>
2017-11-14 14:47:18 +0100

    Zernike fragment shader progress.

--- shaders/zernike.frag.glsl
+++ shaders/zernike.frag.glsl
@@ -22,11 +22,39 @@ uniform uint coefficients_per_voxel;
 
 out vec4 color;
 
+vec2 to_radial(vec2 cartesian)
+{
+  return vec2(length(cartesian), atan(cartesian.y, cartesian.x));
+}
+
+uint factorial(uint n)
+{
+  uint result = 1;
+  for(uint i = 2; i <= n; i++)
+    result *= i;
+  return result;
+}
+float mode(uvec2 nm, float rho)
+{
+  float result = 0.0;
+  for(uint i = 0; i < (nm.x - nm.y) / 2; i++)
+    result += pow(rho, nm.x - 2 * i) * (pow(-1, i) * factorial(nm.x - i)) / (factorial(i) * factorial((nm.x + nm.y) / 2 - i) * factorial((nm.x - nm.y) / 2 - i));
+  return result;
+}
+float evaluate(uvec2 nm, vec2 rt)
+{
+  return mode(nm, rt.x) * (nm.y >= 0 ? cos(nm.y * rt.y) : sin(nm.y * rt.y));
+}
+
 void main()
 {
-  color = vec4(1.0, 0.0, 0.0, 1.0);
+  uint coefficient_offset = fs_in.offset * coefficients_per_voxel;
+  for(uint i = 0; i < coefficients_per_voxel; i++)
+  {
+
+  }
+  color = vec4(1.0, 1.0, 1.0, 1.0);
 }
 )";
 }
-
 #endif
