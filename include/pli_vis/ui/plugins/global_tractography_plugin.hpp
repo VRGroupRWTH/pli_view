8b6f8998c37f0878379d17d94de9a4d680827a26
demiralp <demiralp@vr.rwth-aachen.de>
2017-11-30 18:54:45 +0100

    Added boilerplate for the GT plugin.

--- include/pli_vis/ui/plugins/global_tractography_plugin.hpp
+++ include/pli_vis/ui/plugins/global_tractography_plugin.hpp
@@ -1,7 +1,10 @@
 #ifndef PLI_VIS_GLOBAL_TRACTOGRAPHY_PLUGIN_HPP_
 #define PLI_VIS_GLOBAL_TRACTOGRAPHY_PLUGIN_HPP_
 
+#include <future>
+
 #include <pli_vis/ui/plugin.hpp>
+#include <pli_vis/visualization/algorithms/streamline_renderer.hpp>
 #include <ui_global_tractography_toolbox.h>
 
 namespace pli
@@ -11,8 +14,13 @@ class global_tractography_plugin : public plugin<global_tractography_plugin, Ui_
 public:
   explicit global_tractography_plugin(QWidget* parent = nullptr);
 
+  void start() override;
+  
 protected:
+  void trace();
 
+  std::future<void>    future_;
+  streamline_renderer* streamline_renderer_ = nullptr;
 };
 }
 
